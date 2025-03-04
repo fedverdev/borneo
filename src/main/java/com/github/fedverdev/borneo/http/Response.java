b1a819e199e7a42bf4a854c6ba84a908debd7e68
Fedor Verbitsky fedverdev <fedia.verbitsky@yandex.ru>
2025-03-05 00:07:18 +0700

    added class for response

---
+++ src/main/java/com/github/fedverdev/borneo/http/Response.java
@@ -0,0 +1,35 @@
+package com.github.fedverdev.borneo.http;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Map;
+
+public class Response {
+    private HttpStatus status;
+    private String body;
+    private Map<String, String> headers;
+    private OutputStream outputStream;
+
+    public Response(OutputStream outputStream, HttpStatus status, String body, Map<String, String> headers) {
+        this.outputStream = outputStream;
+        this.status = status;
+        this.body = body;
+        this.headers = headers;
+    }
+
+    public void send() throws IOException {
+        StringBuilder formResponse = new StringBuilder();
+        formResponse.append(String.format("HTTP/1.1 %s %s\r\n", this.status.getCode(), this.status.getMessage()));
+        headers.forEach((key, value) -> formResponse.append(String.format("%s: %s\r\n", key, value)));
+        formResponse.append(String.format("Content-Length: %d\r\n\r\n", this.body.length()));
+        formResponse.append(body);
+        System.out.println(formResponse.toString());
+        outputStream.write(formResponse.toString().getBytes());
+        outputStream.flush();
+    }
+
+    public HttpStatus getStatus() { return status; }
+    public String getBody() { return body; }
+    public Map<String, String> getHeaders() { return headers; }
+    public OutputStream getOutputStream() { return outputStream; }
+}
