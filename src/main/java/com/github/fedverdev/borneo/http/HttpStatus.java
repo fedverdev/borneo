9438776b12b086368aedfb2ff7523cad7f2e681a
Fedor Verbitsky fedverdev <fedia.verbitsky@yandex.ru>
2025-03-04 18:59:27 +0700

    added enum for http statuses

---
+++ src/main/java/com/github/fedverdev/borneo/http/HttpStatus.java
@@ -0,0 +1,36 @@
+package com.github.fedverdev.borneo.http;
+
+import com.github.fedverdev.borneo.exceptions.NotImplementedException;
+
+public enum HttpStatus {
+    OK(200, "OK"),
+    BAD_REQUEST(400, "Bad Request"),
+    UNAUTHORIZED(401, "Unauthorized"),
+    FORBIDDEN(403, "Forbidden"),
+    NOT_FOUND(404, "Not Found"),
+    METHOD_NOT_ALLOWED(405, "Method Not Allowed"),
+    INTERNAL_SERVER_ERROR(500, "Internal Server Error"),
+    NOT_IMPLEMENTED(501, "Not Implemented");
+
+    private final int code;
+    private final String message;
+
+    HttpStatus(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    @Override
+    public String toString() {
+        return code + " " + message;
+    }
+
+}
